7d770f68d1ece700d65911c2b34a02c9a5800ef4
Krizdaniels <dannychris2@gmail.com>
2024-01-16 11:50:37 -0800

    AirBnB Clone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -26,20 +26,25 @@ class FileStorage:
 
     def new(self, obj):
         """ It sets in dictionary the obj with key <obj class name>.id """
-        FileStorage.__objects[obj.__class__.__name__ + "." + str(obj.id)] = obj
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """ It serializes objects to the JSON file (path: __file_path) """
-        with open(FileStorage.__file_path, 'w', encoding='utf-8') as fname:
-            new_dict = {key: obj.to_dict() for key, obj in
-                        FileStorage.__objects.items()}
-            json.dump(new_dict, fname)
+        save_dict = {key: obj.to_dict() for key, obj in
+                     FileStorage.__objects.items()}
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump(save_dict, file)
 
     def reload(self):
-        """ It reload the file """
-        if (os.path.isfile(FileStorage.__file_path)):
-            with open(FileStorage.__file_path, 'r', encoding="utf-8") as fname:
-                l_json = json.load(fname)
-                for key, val in l_json.items():
-                    FileStorage.__objects[key] = eval(
-                        val['__class__'])(**val)
+        """ Load objects from JSON file to __objects. """
+        try:
+            with open(FileStorage.__file_path, 'r') as file:
+                load_dict = json.load(file)
+                for key, obj_dict in load_dict.items():
+                    class_name = obj_dict['__class__']
+                    del obj_dict['__class__']
+                    obj = globals()[class_name](**obj_dict)
+                    FileStorage.__objects[key] = obj
+        except FileNotFoundError:
+            pass
